65604131bb94a88107070490f406670ca12e5f37
kzoltowski@student.wsb-nlu.edu.pl <kzoltowski@debian-Karol.kzoltowski>
2024-02-09 20:06:28 +0100

    Kalkulator działa

--- calc.py
+++ calc.py
@@ -1,2 +1,34 @@
-#Kalkulator
+
+# Kalkulator
+
+import sys
+
+if len(sys.argv) != 4:
+    print("Uzycie: calc.py <num1> <op +-> <num2>")
+    sys.exit(1)
+
+
+num1, op, num2 = sys.argv[1], sys.argv[2], sys.argv[3]
+
+
+num1 = float(num1)
+num2 = float(num2)
+
+
+result = None
+
+
+if op == "+":
+    result = num1 + num2
+elif op == "-":
+    result = num1 - num2
+else:
+    print("Zly operator")
+    sys.exit(1)
+
+
+with open("/tmp/wynik.txt", "w") as file: 
+    file.write(str(result))
+
+print("wynik {}".format(result))
 
